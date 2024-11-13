286ff80f3a156ac28ebb9b1ba2d9af21014bad0e
miro789 <mirohuyen5335.em@gmail.com>
2024-11-14 00:18:34 +0700

    update app & jdbc

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,17 +1,9 @@
 package com.example.demo;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.example.demo.run.Location;
-import com.example.demo.run.Run;
 
 @SpringBootApplication
 public class Application {
@@ -33,14 +25,15 @@ public class Application {
 //		System.out.println(welcomeMessage.getWelcomeMessage());
 	}
 
-	@Bean
-	CommandLineRunner runner() {
-		return args -> {
-			Run run = new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 3,
-					Location.OUTDOOR);
-			log.info("Run: " + run);
-		};
-
-	}
+//	@Bean
+//	CommandLineRunner runner(RunRepository runRepository) {
+//		return args -> {
+//			Run run = new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 3,
+//					Location.OUTDOOR);
+//			runRepository.create(run);
+////			log.info("Run: " + run);
+//		};
+//
+//	}
 
 }
--- src/main/java/com/example/demo/run/Run.java
+++ src/main/java/com/example/demo/run/Run.java
@@ -2,6 +2,14 @@ package com.example.demo.run;
 
 import java.time.LocalDateTime;
 
-public record Run(Integer id, String title, LocalDateTime startedOn, LocalDateTime completedOn, Integer miles,
-		Location location) {
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
+public record Run(Integer id, @NotEmpty String title, LocalDateTime startedOn, LocalDateTime completedOn,
+		@Positive Integer miles, Location location) {
+	public Run {
+		if (!completedOn.isAfter(startedOn)) {
+			throw new IllegalArgumentException("Completed On must be after Started On");
+		}
+	}
 }
